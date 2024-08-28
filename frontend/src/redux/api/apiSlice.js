3a5870338016b6690bc17a4adc34bcb98b158006
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-08-28 15:07:01 +0300

    setting up redux query and creating our apiSlice

---
+++ frontend/src/redux/api/apiSlice.js
@@ -0,0 +1,9 @@
+import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
+
+const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:9000" });
+
+export const apiSlice = createApi({
+  baseQuery,
+  tagTypes: ["User", "Product"],
+  endpoints: () => ({}),
+});
