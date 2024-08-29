11a3b354ea467f3d7aa4bba4ef976665e406af5f
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-08-29 04:09:27 +0300

    getAllUsers route and little precaution for getting password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,11 +5,24 @@ export const getUser = async (req, res, next) => {
   try {
     const { userId } = req.params;
 
-    const user = await User.findById(userId);
+    const user = await User.findById(userId).select("-password");
 
     if (!user) return next(errorHandler(404, "User not found"));
 
     res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ error: `Error in auth register: ${error.message}` });
+    console.log(error.message);
+  }
+};
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({}).select("-password");
+
+    if (!users) return next(errorHandler(404, "Users not found"));
+
+    res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: `Error in auth register: ${error.message}` });
     console.log(error.message);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getUser } from "../controllers/userController.js";
+import { getAllUsers, getUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/:userId", getUser);
+router.get("/", getAllUsers);
 
 export default router;
